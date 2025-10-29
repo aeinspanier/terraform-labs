42a8abe074f97ff05f5ef889b62efc98bdab389f
aeinspanier <72575207+aeinspanier@users.noreply.github.com>
2025-10-29 07:46:57 -0500

    modified server with ssh key

---
+++ providers.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  profile    = "terraform-user"
+  access_key = ""
+  secret_key = ""
+}
---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "aws_region" {
+  type        = string
+  description = "region used to deploy workloads"
+  default     = "us-east-1"
+  validation {
+    condition     = can(regex("^us-", var.aws_region))
+    error_message = "The aws_region value must be a valid region in the USA, starting with \"us-\"."
+  }
+}
+
+variable "environment" {
+  description = "Current Environment"
+  type        = string
+  default     = "prod"
+}
+
+variable "variables_sub_cidr" {
+  description = "CIDR Block for the Variables Subnet"
+  type        = string
+  default     = "10.0.202.0/24"
+}
+
+variable "variables_sub_az" {
+  description = "Availability Zone used Variables Subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "variables_sub_auto_ip" {
+  description = "Set Automatic IP Assignment for Variables Subnet"
+  type        = bool
+  default     = true
+}
+
+
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
+}
+
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
+
